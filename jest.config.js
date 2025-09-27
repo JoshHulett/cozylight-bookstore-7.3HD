da7ffbf15e03ef8d31caaebd50b7e64225c910a7
JoshHulett <josh.hulett@outlook.com>
2025-09-27 16:18:24 +1000

    Update ignored dev files in coverage report

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,13 @@ module.exports = {
     collectCoverage: true,
     coverageReporters: ["lcov", "text"],
     coverageDirectory: '/app/coverage',
+    coveragePathIgnorePatterns: [
+        "/public_html",
+        "/tests",
+        "jest.config.js",
+        "createBookDB.js",
+        "createEnquiryDB.js"
+    ],
     reporters: [
         'default',
         ['jest-junit', {
